839f7ea8a514cb609f8263c7057723079962b405
vsoch <vsochat@stanford.edu>
2015-07-25 16:41:58 -0700

    modified:   index.py 	modified:   templates/index.html

--- index.py
+++ index.py
@@ -16,26 +16,52 @@ def get_behavior(trait):
 def show_about():
     return render_template('about.html')
 
+def get_behaviors_df():
+    # Read in behavioral terms and cattell terms
+    traits = load_behaviors()
+    cattell = pandas.read_pickle("data/pickle/cattell_personality_282.pkl").trait.unique().tolist()
+    df = pandas.DataFrame()
+    df["traits"] =  [x.replace("\xc3\xaf","i") for x in traits + cattell]
+    df["sources"] = ["CNP" for x in range(len(traits))] + ["cattell" for x in range(len(cattell))]
+    df["ids"] = [x.replace(" ","").replace("(","").replace(")","") for x in df.traits]
+    return df.sort(columns="traits")
+ 
+def get_prettyjson(behavior):
+    if len(behavior.is_a) == 0:   
+        myjson = {"nodes":[{"name":behavior.trait,"definition":""}],"links":[]}
+    else:
+        myjson = get_json(behavior)
+    return json.dumps(myjson, sort_keys=True, indent=4, separators=(',', ': '))
+
+def render_index(trait,df):
+    behavior = get_behavior(trait)
+    prettyjson = get_prettyjson(behavior)
+    behaviors = zip(df.traits.tolist(),range(len(df.index)),df.ids.tolist(),df.sources.tolist())
+
+    return render_template('index.html',prettyjson=prettyjson,behavior=behavior.trait,behaviors=behaviors)
+
+   
+@app.route('/view/<traitid>')
+def select_traits(traitid):
+    df = get_behaviors_df()
+    
+    print df.columns
+    print df
+    # Find the selected trait
+    print df["traits"][df["ids"] == traitid]
+    
+    trait = str(df["traits"][df["ids"] == traitid].tolist()[0])
+    return render_index(trait,df)
 
 @app.route('/')
 def explore_traits():
     
-    # Read in behavioral terms and cattell terms
-    traits = load_behaviors()
-    cattell = pandas.read_pickle("data/pickle/cattell_personality_282.pkl").trait.unique().tolist()
-    behaviors = [x.replace("\xc3\xaf","i") for x in traits + cattell]
-        
+    df = get_behaviors_df()
+    behaviors = zip(df.traits.tolist(),range(len(df.index)),df.ids.tolist(),df.sources.tolist())    
 
     # Generate Behavior object with related terms
-    behavior = get_behavior(random.sample(traits,1)[0])
-    if len(behavior.is_a) == 0:   
-        myjson = {"nodes":[{"name":behavior.trait}],"links":[]}
-    else:
-        myjson = get_json(behavior)
-    prettyjson = json.dumps(myjson, sort_keys=True, indent=4, separators=(',', ': '))
-
-    return render_template('index.html',prettyjson=prettyjson,behavior=behavior.trait,behaviors=behaviors)
-
+    trait = random.sample(df.traits.tolist(),1)[0]
+    return render_index(trait,df)
 
 @app.route('/cattell')
 def show_traits():
